243d33f5fcb73fa65deded14e7571c81968c20f5
Mia Borja <apk.borja@gmail.com>
2022-06-01 19:47:27 -0700

    Updated variables for routes and placeholder code.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { Post } = require ('../../models');
+const Auth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
     const TEST = 'test';
 });
 
+router.post('/', )
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,35 @@
 const router = require('express').Router();
-const { User, Post } = require('../models')
+const { User, Post } = require('../models');
+const Auth = require('../utils/auth');
 
+router.get('/', Auth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            //
+        });
+
+        const dashPosts = postData.map((posts) =>
+            posts.get()
+        );
+        res.render('dashboard', {
+            //,
+        });
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+})
 
 module.exports = router;
