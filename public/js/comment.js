f56780c31357eef1459cdaef41938ddac36dc830
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-06-01 20:56:53 -0700

    removing signup.js file

--- public/js/comment.js
+++ public/js/comment.js
@@ -12,8 +12,8 @@ async function commentFormHandler(event) {
         const response = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({
-                post_id,
-                comment_text
+                id,
+                body
             }),
             headers: {
                 'Content-Type': 'application/json'
