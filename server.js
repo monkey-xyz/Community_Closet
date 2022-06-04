cef9ef4a252eee2c9c8d1d2544a3f7b933751d11
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-06-04 11:06:27 -0700

    handlebars work

--- server.js
+++ server.js
@@ -30,6 +30,9 @@ app.use(session(sess));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+//images folder?
+app.use(express.static("images"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
