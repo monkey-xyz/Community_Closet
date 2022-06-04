e6b0aed916af2ed52cf190a2aaee534f474cbbd3
Kiersten Zug <99204084+Kzug@users.noreply.github.com>
2022-06-04 11:17:18 -0700

    Added s3 portion to server.js

--- models/Picture.js
+++ models/Picture.js
@@ -1,35 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Picture extends Model { }
+class Picture extends Model {}
 
 Picture.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-          },
-        url: {
-            type: DataTypes.sting,
-            allowNull: true,
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'post',
-                key: 'id',
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'Picture',
-    }
+    url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Picture",
+  }
 );
 
 module.exports = Picture;
--- server.js
+++ server.js
@@ -39,3 +39,57 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
+
+//setting up S3
+// var express = require("express"),
+var aws = require("aws-sdk"),
+  bodyParser = require("body-parser"),
+  multer = require("multer"),
+  multerS3 = require("multer-s3");
+
+aws.config.update({
+  secretAccessKey: process.env.AWS_ACCESS_KEY_ID,
+  accessKeyId: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "us-west-2",
+  signatureVersion: "v4",
+});
+
+s3 = new aws.S3();
+
+app.use(bodyParser.json());
+
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: "community-closet-s3-bucket",
+    key: function (req, file, cb) {
+      console.log(file);
+      cb(null, file.originalname); //use Date.now() for unique file keys
+    },
+  }),
+});
+
+//open in browser to see upload form
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html"); //index.html is inside node-cheat
+});
+
+//use by upload form
+app.post("/upload", upload.array("upl", 25), function (req, res, next) {
+  res.send({
+    message: "Uploaded!",
+    urls: req.files.map(function (file) {
+      return {
+        url: file.location,
+        name: file.key,
+        type: file.mimetype,
+        size: file.size,
+      };
+    }),
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
