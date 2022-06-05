1682b65790921dbfe8ed73af927a39f0aaeeed53
Kiersten Zug <99204084+Kzug@users.noreply.github.com>
2022-06-04 23:04:15 -0700

    Login page now displays options to sign in and sign up. Sign up is working properly

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,29 +1,42 @@
-const router = require('express').Router();
-const { User } = require('../../models')
+const router = require("express").Router();
+const { User } = require("../../models");
 
-router.post('/login', async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
-    } catch (err) {
-        res.status(404).json(userData);
-    }
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(404).json(err);
+  }
 
-    // Update post route for creating a user. Check for email, password, etc.
+  // Update post route for creating a user. Check for email, password, etc.
 });
 
-router.get('/', async (req, res) => {
-    try {
-        const userData = await User.findAll({
-            exclude: [
-                {
-                    attributes: ['password'],
-                }
-            ]
-        });
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(404).json(userData);
-    }
+//route that lets a user sign up
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(404).json(err);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      exclude: [
+        {
+          attributes: ["password"],
+        },
+      ],
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(404).json(userData);
+  }
 });
 
 // Create post route for logging in. Check table for an existing user fulfilling the login info.
