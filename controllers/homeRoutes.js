68df694b67e22d61153982801f0cc36e78bc65dd
Mia Borja <apk.borja@gmail.com>
2022-06-06 19:11:45 -0700

    Fixed login button not being replaced after login.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,7 @@ const Auth = require("../utils/auth");
 // When server is ready, test the routes and update them with working code.
 // 1st get route should return all posts for the page, 2nd should work as a singular post page, 3rd should provide the user profile. 4th should provide Login page upon use.
 
-router.get("/homepage", Auth, async (req, res) => {
+router.get("/homepage", async (req, res) => {
   try {
     const postData = await Post.findAll({
       include: [
@@ -23,6 +23,7 @@ router.get("/homepage", Auth, async (req, res) => {
 
     res.render("homepage", {
       posts,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
