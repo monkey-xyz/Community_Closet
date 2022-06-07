54ea8c09486685cfdd36be500bfa08112097d49e
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-06-06 21:28:16 -0700

    fixing comment model to include post and user id

--- models/Comment.js
+++ models/Comment.js
@@ -15,6 +15,26 @@ Comment.init(
             type: DataTypes.STRING,
             allowNull: true,
         },
+        post_id: 
+        {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: 
+            {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        user_id: 
+        {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: 
+            {
+                model: 'user',
+                key: 'id'
+            }
+        }
     },
     {
         sequelize,
