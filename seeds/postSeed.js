5be3872390eca81c6cab0e9286400f4aa41ae0a8
April Reitan <45362322+areitan@users.noreply.github.com>
2022-06-06 21:24:51 -0700

    adding to post seed file

--- seeds/postSeed.js
+++ seeds/postSeed.js
@@ -3,22 +3,30 @@ const { Post } = require('../models');
 const posts = [{
         title: 'Zombies!',
         description: 'Zombies reversus ab inferno, nam malum cerebro.',
+        location: Tacoma,
+        size: Large,
         user_id: 1
 
     },
     {
         title: 'Opportunity',
         description: 'Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis.',
+        location: Auburn,
+        size: Small,
         user_id: 2
     },
     {
         title: 'Zombie Ipsum',
         description: 'Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. ',
+        location: SeaTac,
+        size: Small,
         user_id: 3
     },
     {
         title:"yarg",
         description: "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast.",
+        location: Bellevue,
+        size: Medium,
         user_id: 1
     }
 ];
