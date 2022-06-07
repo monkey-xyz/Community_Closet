7216e2bcb35cb7dc271db19f70a5a7d2db7ec8b2
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-06-06 21:09:58 -0700

    fixing post comment.js file

--- seeds/commentSeed.js
+++ seeds/commentSeed.js
@@ -2,11 +2,16 @@ const { Comment } = require('../models');
 
 const comments = [{
     id: 1,
+    post_id: 2,
+    user_id :3 ,
     body: "lorem ipsum"
 },
 {
     id: 2,
+    post_id: 3,
+    user_id : 5,
     body: "lorem ipsum"
+    
 }]
 
 const seedComments = () => Comment.bulkCreate(comments);
