f31bd9791a0bd398de780b04f006e92b5e6a9d7b
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-06-08 21:39:23 -0700

    Update pictureRoutes.js

--- controllers/api/pictureRoutes.js
+++ controllers/api/pictureRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post } = require("../../models");
+const { Picture, Post } = require("../../models");
 const Auth = require("../../utils/auth");
 const upload = require("../../s3");
 
@@ -8,6 +8,11 @@ router.post("/upload", upload.single("upl"), async function (req, res, next) {
     res.status(400).json({ Error: "Choose an image file" });
     return;
   }
+
+  // const post_id = req.params.id;
+  // const uploadedImage = `
+  // <img src=${req.file.location} width="200px" />
+  // `;
   
 await Post.create( {
   title: req.body["post-title"],
@@ -21,6 +26,26 @@ res.redirect('/profile')
 console.log(req.file.location);
 console.log(req.body);
 });
+
+
+
+// Post route for table? Link the Picture model up to a post_id. Put route maybe?
+
+// router.post ('/', async (req, res) => {
+//     try {
+//         const pictureData = await Picture.findAll({
+//             include: [
+//                 {
+//                     model: Post,
+//                 },
+//             ],
+//         });
+
+//         res.status(200).json(pictureData);
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
 
 // router.get("/", async (req, res) => {
 //   try {
