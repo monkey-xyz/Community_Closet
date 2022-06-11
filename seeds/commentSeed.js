a1f37ccc9940c6df3a6385e92c98b7861493764c
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-06-11 13:25:03 -0700

    final pull

--- seeds/commentSeed.js
+++ seeds/commentSeed.js
@@ -4,13 +4,20 @@ const comments = [{
     id: 1,
     post_id: 2,
     user_id: 3,
-    body: "lorem ipsum",
+    body: "Adrian, email@gmail.com, please contact me if this item is still available!",
 },
 {
     id: 2,
     post_id: 3,
     user_id : 3,
-    body: "lorem ipsum",
+    body: "Looks Awesome! Im interested!",
+    
+},
+{
+    id: 3,
+    post_id: 4,
+    user_id : 4,
+    body: "When Can I Pick up this item?",
     
 }]
 
