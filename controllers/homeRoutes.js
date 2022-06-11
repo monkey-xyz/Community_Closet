5ce45375720c318bef68ca7f9003c55d0002708a
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-06-11 09:13:11 -0700

    fixing login issue

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,7 @@ const Auth = require("../utils/auth");
 // When server is ready, test the routes and update them with working code.
 // 1st get route should return all posts for the page, 2nd should work as a singular post page, 3rd should provide the user profile. 4th should provide Login page upon use.
 
-router.get("/homepage", async (req, res) => {
+router.get("/homepage", Auth, async (req, res) => {
   try {
     const postData = await Post.findAll({
       include: [
